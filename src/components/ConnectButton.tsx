505a3e7fbf135e2be9e17c906144a1eaf3a2b077
SullFurix <39709633+SullFurix@users.noreply.github.com>
2021-09-29 16:34:26 +0200

    Add files via upload

---
+++ src/components/ConnectButton.tsx
@@ -0,0 +1,84 @@
+import { Button, Box, Text } from "@chakra-ui/react";
+import { useEthers, useTokenBalance } from "@usedapp/core";
+import { formatEther } from "@ethersproject/units";
+import { SFXJSON } from "../constants/artifacts";
+import Identicon from "./Identicon";
+
+type Props = {
+  handleOpenModal: any;
+};
+
+export default function ConnectButton({ handleOpenModal }: Props) {
+  const { activateBrowserWallet, account, chainId } = useEthers();
+
+  const sfxBalance = useTokenBalance(SFXJSON.contractAddress, "0x9be948033e0430a0747bb38316f8d9173ba93676");
+
+  function handleConnectWallet() {
+    activateBrowserWallet();
+  }
+
+
+  console.log(chainId);
+
+  return account ? (
+    <>
+    <Box
+      display="flex"
+      alignItems="center"
+      background="gray.700"
+      borderRadius="xl"
+      py="0"
+    >
+      <Box px="3">
+        <Text color="white" fontSize="md">
+          {sfxBalance && parseFloat(formatEther(sfxBalance)).toFixed(0)} SFX
+        </Text>
+      </Box>
+      <Button
+        onClick={handleOpenModal}
+        bg="gray.800"
+        border="1px solid transparent"
+        _hover={{
+          border: "1px",
+          borderStyle: "solid",
+          borderColor: "blue.400",
+          backgroundColor: "gray.700",
+        }}
+        borderRadius="xl"
+        m="1px"
+        px={3}
+        height="38px"
+      >
+        <Text color="white" fontSize="md" fontWeight="medium" mr="2">
+          {account &&
+            `${account.slice(0, 6)}...${account.slice(
+              account.length - 4,
+              account.length
+            )}`}
+        </Text>
+        <Identicon />
+      </Button>
+    </Box>
+    </>
+  ) : (
+    <Button
+      onClick={handleConnectWallet}
+      bg="blue.800"
+      color="blue.300"
+      fontSize="lg"
+      fontWeight="medium"
+      borderRadius="xl"
+      border="1px solid transparent"
+      _hover={{
+        borderColor: "blue.700",
+        color: "blue.400",
+      }}
+      _active={{
+        backgroundColor: "blue.800",
+        borderColor: "blue.700",
+      }}
+    >
+      Connect to a wallet
+    </Button>
+  );
+}
